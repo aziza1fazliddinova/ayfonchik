9992c139e1dce8b0fe1533163a7c973405d3de14
aziza <azizafazliddinova08@gmail.com>
2023-12-12 16:19:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let button1 = document.querySelector('.Natural_Titanium')
+let button2 = document.querySelector('.Blue_Titanium')
+let button3 = document.querySelector('.White_Titanium')
+let button4 = document.querySelector('.Black_Titanium')
+
+let img1 = document.querySelector('.aziza_images')
+let img2 = document.querySelector('.natural')
+let img3 = document.querySelector('.blue')
+let img4 = document.querySelector('.white')
+let img5 = document.querySelector('.black')
+
+let h1 = document.querySelector('color_text2')
+
+button1.onclick = () => {
+    img1.classList = 'img2 natural'
+}
+
+button2.onclick = () => {
+    img1.classList = 'img3 blue'
+    
+
+}
+
+button3.onclick = () => {
+    img1.classList = 'img4 white'
+    
+}
+
+button4.onclick = () => {
+    img1.classList = 'img5 black'
+}
